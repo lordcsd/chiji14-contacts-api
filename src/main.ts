37981a15999fbc6ccdb948cd82a3c29d217425bb
Chinonso Sunday Dimgba <dimgbachinonso@gmail.com>
2021-10-10 05:42:54 +0100

    Implemented heroku changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+//  app.setGlobalPrefix('api');
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Contacts Api')
